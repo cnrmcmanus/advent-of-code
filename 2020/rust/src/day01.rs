1d66a398125d4bb99c392120f6734c05fa5ee82b
Conor McManus <cnrmcmanus@gmail.com>
2024-07-26 19:33:54 +0100

    feat(rust2020): add solution for day 01

---
+++ 2020/rust/src/day01.rs
@@ -0,0 +1,33 @@
+use crate::util::*;
+use std::cmp::Ordering;
+
+fn find_pair_sum(numbers: &[u32], goal: u32) -> Option<u32> {
+    for i in 0..numbers.len() {
+        let x = numbers[i];
+        for &y in &numbers[i + 1..] {
+            match (x + y).cmp(&goal) {
+                Ordering::Equal => return Some(x * y),
+                Ordering::Greater => break,
+                Ordering::Less => {}
+            }
+        }
+    }
+    None
+}
+
+fn find_triple_sum(numbers: &[u32], goal: u32) -> Option<u32> {
+    for i in 0..numbers.len() {
+        let x = numbers[i];
+        if let Some(partial) = find_pair_sum(&numbers[i + 1..], goal - x) {
+            return Some(x * partial);
+        }
+    }
+    None
+}
+
+pub fn main() {
+    let mut numbers: Vec<u32> = stdin_lines().filter_map(|n| n.parse().ok()).collect();
+    numbers.sort();
+    println!("{}", find_pair_sum(&numbers, 2020).unwrap_or_default());
+    println!("{}", find_triple_sum(&numbers, 2020).unwrap_or_default());
+}
--- 2020/rust/src/main.rs
+++ 2020/rust/src/main.rs
@@ -1,8 +1,10 @@
+mod day01;
 mod util;
 
 fn main() {
     let day = std::env::args().nth(1).unwrap_or_default();
     match day.as_str() {
+        "01" => day01::main(),
         _ => {}
     }
 }
