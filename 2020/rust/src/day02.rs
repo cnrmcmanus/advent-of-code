1b9cca9551fa6af2365d1d3ba4fc052b9aff585b
Conor McManus <cnrmcmanus@gmail.com>
2024-07-26 23:47:14 +0100

    feat(rust2020): add solution for day 02

---
+++ 2020/rust/src/day02.rs
@@ -0,0 +1,41 @@
+use crate::util::*;
+use regex::Regex;
+
+struct Policy {
+    c: char,
+    x: usize,
+    y: usize,
+    passcode: String,
+}
+
+impl Policy {
+    fn new(re: &Regex, text: &str) -> Option<Policy> {
+        let m = re.captures(text)?;
+        Some(Policy {
+            x: m.get(1)?.as_str().parse().ok()?,
+            y: m.get(2)?.as_str().parse().ok()?,
+            c: m.get(3)?.as_str().chars().nth(0)?,
+            passcode: m.get(4)?.as_str().to_string(),
+        })
+    }
+
+    fn match_range(&self) -> bool {
+        let n = self.passcode.chars().filter(|&c| c == self.c).count();
+        n >= self.x && n <= self.y
+    }
+
+    fn match_indexes(&self) -> bool {
+        let a = self.passcode.chars().nth(self.x - 1);
+        let b = self.passcode.chars().nth(self.y - 1);
+        (a == Some(self.c)) ^ (b == Some(self.c))
+    }
+}
+
+pub fn main() {
+    let re = Regex::new(r"(\d+)-(\d+) (.): (.*)").unwrap();
+    let policies: Vec<Policy> = stdin_lines()
+        .filter_map(|line| Policy::new(&re, &line))
+        .collect();
+    println!("{}", policies.iter().filter(|p| p.match_range()).count());
+    println!("{}", policies.iter().filter(|p| p.match_indexes()).count());
+}
--- 2020/rust/src/main.rs
+++ 2020/rust/src/main.rs
@@ -1,10 +1,12 @@
 mod day01;
+mod day02;
 mod util;
 
 fn main() {
     let day = std::env::args().nth(1).unwrap_or_default();
     match day.as_str() {
         "01" => day01::main(),
+        "02" => day02::main(),
         _ => {}
     }
 }
