b51fb70878a828746ee770dc94556359e4d4cd05
Conor McManus <cnrmcmanus@gmail.com>
2025-12-01 12:50:34 +0000

    feat(rust2025): update day 01 with learnings from haskell solution

--- 2025/rust/src/day01.rs
+++ 2025/rust/src/day01.rs
@@ -1,23 +1,22 @@
 use utils::*;
 
-fn rotate(pos: u32, dir: &str, amount: u32) -> (u32, u32) {
-    let pos_rotated = if dir == "L" { 100 - pos } else { pos } + amount;
-    let score = (pos_rotated / 100) - u32::from(pos == 0 && dir == "L");
-    let new_pos = pos_rotated % 100;
-    let new_pos = if dir == "L" { 100 - new_pos } else { new_pos } % 100;
-    (new_pos, score)
+fn turn(pos: i32, by: i32) -> (i32, i32) {
+    let passes = (pos + by).div_euclid(100).abs();
+    let new_pos = (pos + by).rem_euclid(100);
+    let (left_start_zero, left_end_zero) = (pos == 0 && by < 0, new_pos == 0 && by < 0);
+    (new_pos, passes - i32::from(left_start_zero) + i32::from(left_end_zero))
 }
 
 pub fn main() {
     let mut pos = 50;
     let (naive, click_method) = stdin_lines().map_sum_tuple(|line| {
         let (dir, amount_str) = line.split_at(1);
-        let amount = amount_str.parse::<u32>().unwrap();
+        let amount = amount_str.parse::<i32>().unwrap();
 
-        let (new_pos, score) = rotate(pos, dir, amount);
+        let (new_pos, score) = turn(pos, amount * (if dir == "L" { -1 } else { 1 }));
         pos = new_pos;
 
-        (u32::from(pos == 0), score)
+        (i32::from(pos == 0), score)
     });
     println!("{}\n{}", naive, click_method);
 }
